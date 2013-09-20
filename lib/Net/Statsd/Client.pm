338109af49d74187df9d7d8e95c9f338799b8634
Andrew Rodland <arodland@shutterstock.com>
2013-09-20 11:07:47 -0400

    Revert "Merge branch 'refactor/generic-metrics'"
    This reverts commit a7f55b1714a51f767550e3210051035464acb1c2, reversing
    changes made to c45f28d534c8788c7a62eb9fae320393d1c8d2eb.

--- lib/Net/Statsd/Client.pm
+++ lib/Net/Statsd/Client.pm
@@ -44,43 +44,43 @@ sub BUILD {
   );
 }
 
-sub _send {
-  my ($self, $data, $sample_rate) = @_;
-
-  $self->{statsd}->send(
-    { "$self->{prefix}$data->{metric}" => "$data->{value}|$data->{type}" },
-    ( defined($data->{sample_rate}) ? $data->{sample_rate} : $self->{sample_rate} ),
-  );
-}
-    
 sub increment {
-  # ($self, $metric, [$sample_rate])
-  $_[0]->_send({ metric => $_[1], value => 1, type => "c", sample_rate => $_[2] });
+  my ($self, $metric, $sample_rate) = @_;
+  $metric = "$self->{prefix}$metric";
+  $sample_rate = $self->{sample_rate} unless defined $sample_rate;
+  $self->{statsd}->increment($metric, $sample_rate);
 }
 
 sub decrement {
-  # ($self, $metric, [$sample_rate])
-  $_[0]->_send({ metric => $_[1], value => -1, type => "c", sample_rate => $_[2] });
+  my ($self, $metric, $sample_rate) = @_;
+  $metric = "$self->{prefix}$metric";
+  $sample_rate = $self->{sample_rate} unless defined $sample_rate;
+  $self->{statsd}->decrement($metric, $sample_rate);
 }
 
 sub update {
-  # ($self, $metric, $value, [$sample_rate])
-  $_[0]->_send({ metric => $_[1], value => $_[2], type => "c", sample_rate => $_[3] });
+  my ($self, $metric, $value, $sample_rate) = @_;
+  $metric = "$self->{prefix}$metric";
+  $sample_rate = $self->{sample_rate} unless defined $sample_rate;
+  $self->{statsd}->update($metric, $value, $sample_rate);
 }
 
 sub timing_ms {
-  # ($self, $metric, $time, [$sample_rate])
-  $_[0]->_send({ metric => $_[1], value => $_[2], type => "ms", sample_rate => $_[3] });
+  my ($self, $metric, $time, $sample_rate) = @_;
+  $metric = "$self->{prefix}$metric";
+  $self->{statsd}->timing($metric, $time, $sample_rate);
 }
 
 sub gauge {
-  # ($self, $metric, $value, [$sample_rate])
-  $_[0]->_send({ metric => $_[1], value => $_[2], type => "g", sample_rate => $_[3] });
+  my ($self, $metric, $value, $sample_rate) = @_;
+  $metric = "$self->{prefix}$metric";
+  $self->{statsd}->send({ $metric => "$value|g" }, $sample_rate);
 }
 
 sub set_add {
-  # ($self, $metric, $value, [$sample_rate])
-  $_[0]->_send({ metric => $_[1], value => $_[2], type => "s", sample_rate => $_[3] });
+  my ($self, $metric, $value, $sample_rate) = @_;
+  $metric = "$self->{prefix}$metric";
+  $self->{statsd}->send({ $metric => "$value|s" }, $sample_rate);
 }
 
 sub timer {
